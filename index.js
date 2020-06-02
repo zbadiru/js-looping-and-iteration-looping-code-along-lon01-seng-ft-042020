23a8e7eb1c8f72714e808de6d51db9238430f6af
zbadiru <zbadiru@live.co.uk>
2020-06-02 21:20:54 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(nameArray, event){
+    let thankYouCards = []
+    for (let i = 0; i < nameArray.length; i++){
+        thankYouCards.push(`Thank you, ${nameArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCards
+}
+
+function countDown( input){
+    while (input > 0) {
+        console.log(input);
+        input -= 1
+    }
+    console.log(input);
+}
